bfd267ea72ce8431515d9d0829af3bf5165bb7e5
Fellipe Nunes <fellipenunes@Macbook-Pro-de-Fellipe-Nunes.local>
2020-12-26 20:56:24 -0300

    fix

--- src/config/config.js
+++ src/config/config.js
@@ -2,9 +2,7 @@ import axios from 'axios'
 import { getToken } from './auth'
 
 const clientHttp = axios.create({
-    baseURL: process.env.REACT_APP_API_URL //process.env.NODE_ENV ==='development'
-    //? `http://localhost:3001`
-    //: process.env.REACT_APP_API
+    baseURL: `http://localhost:3001` || process.env.REACT_APP_API_URL
 })
 
 clientHttp.defaults.headers['Content-Type'] = 'application/json'
